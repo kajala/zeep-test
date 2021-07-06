67d73e6ea276a55173ff2092dd55d939ff6f7152
Jani Kajala <kajala@gmail.com>
2021-07-06 18:00:07 -0500

    README

---
+++ README.md
@@ -0,0 +1,10 @@
+Minimal test for BinarySignature
+
+Install and run: 
+
+        python3 -m venv venv
+        source venv/bin/activate
+        pip install --upgrade pip
+        pip install -r requirements.txt
+        python test.py
+
--- test.py
+++ test.py
@@ -6,3 +6,15 @@ envelope = etree.fromstring(body_bytes)
 binary_signature = BinarySignature('./key.pem', './cert.pem')
 envelope, soap_headers = binary_signature.apply(envelope, {})
 signed_body_bytes = etree.tostring(envelope)
+"""
+------------------------------------------------------------------------------------ OUTPUT BEGIN
+Traceback (most recent call last):
+  File "test.py", line 7, in <module>
+    envelope, soap_headers = binary_signature.apply(envelope, {})
+  File "/zeep-test/venv/lib/python3.8/site-packages/zeep/wsse/signature.py", line 104, in apply
+    _sign_envelope_with_key_binary(
+  File "/zeep-test/venv/lib/python3.8/site-packages/zeep/wsse/signature.py", line 287, in _sign_envelope_with_key_binary
+    bintok.text = x509_data.find(QName(ns.DS, "X509Certificate")).text
+AttributeError: 'NoneType' object has no attribute 'text'
+------------------------------------------------------------------------------------ OUTPUT END
+"""
